22f9d291e9945e8a159cef41bafd1938a512c0d1
Sara Alves <sara.rodrigues@picpay.com>
2022-06-19 22:56:09 -0300

    commit inicial

---
+++ app/src/main/java/com/saraalves/thestarwars/SplashActivity.kt
@@ -0,0 +1,32 @@
+package com.saraalves.thestarwars
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window.FEATURE_NO_TITLE
+import androidx.core.content.ContextCompat
+import com.saraalves.thestarwars.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivitySplashBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportRequestWindowFeature(FEATURE_NO_TITLE)
+        setContentView(binding.root)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.gray_01)
+//        animaImagem()
+    }
+
+    fun animaImagem() {
+        binding.imgSplash.alpha = 0f
+        binding.imgSplash.y = 900f
+
+        binding.imgSplash.animate()
+            .alpha(1f)
+            .y(250f)
+            .setDuration(2000)
+            .setListener(null)
+    }
+}
